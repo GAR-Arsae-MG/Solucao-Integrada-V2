39689b36396a6a7348e3fc3c3b42719bd9745854
darlanZero <105607085+darlanZero@users.noreply.github.com>
2023-12-27 12:07:18 -0300

    register

--- Frontend/src/_auth/CadastroUsuários.tsx
+++ Frontend/src/_auth/CadastroUsuários.tsx
@@ -1,10 +1,71 @@
-import { SignUpvalidation } from '../../validation/FormValidation'
+import { Card, CardBody, Input, Tab, Tabs } from '@nextui-org/react'
+import { SignUpValidation } from '../../validation/FormValidation'
+import { useState } from 'react'
 
 const CadastroUsuários = () => {
+
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleEmail = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setEmail(event.target.value)
+  }
+
+  const handleName = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setName(event.target.value)
+  }
+
+  const handlePassword = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setPassword(event.target.value)
+  }
+
   return (
-    <div className="flex flex-col w-full items-center gap-4 p-4 min-h-screen">
-        <h1>olá</h1>
-    </div>
+    <>
+      <div>
+          <Card className='w-[1200px] h-[800px] max-w-ful'>
+            <CardBody className='overflow-auto scrollbar-hide'>
+              <Tabs
+                fullWidth
+                size='md'
+                aria-label='Cadastro'
+              >
+                <Tab
+                  key='cadastro'
+                  title='Cadastro de Usuário'
+                  className='flex flex-row justify-between gap-4 p-4'
+                >
+                  <div className=' flex flex-col p-4 gap-4 items-center justify-center'>
+                    <h1 className='font-bold w-full'>Sem Acesso? Sem problema!</h1>
+                    <form>
+                        <Input 
+                          label='Nome'
+                          placeholder='Digite seu nome'
+                          onChange={handleName}
+                          required
+                        />
+
+                        <Input 
+                          label='E-mail'
+                          placeholder='Digite seu e-mail'
+                          onChange={handleEmail}
+                          required
+                        />
+
+                        <Input 
+                          label='Senha'
+                          placeholder='Digite sua senha'
+                          onChange={handlePassword}
+                          required
+                        />
+                    </form>
+                  </div>
+                </Tab>
+              </Tabs>
+            </CardBody>
+          </Card>
+      </div>
+    </>
   )
 }
 
--- Frontend/validation/FormValidation.ts
+++ Frontend/validation/FormValidation.ts
@@ -1,12 +1,12 @@
 import * as z from 'zod'
 
-export const SignUpvalidation = z.object({
+export const SignUpValidation = z.object({
     name: z.string().min(2, { message: 'Nome Inválido' }),
     email: z.string().min(2, { message: 'Um Email é obrigatório' }).email({ message: 'Email inválido' }),
     password: z.string().min(6, { message: 'Sua senha precisa ter ao menos 6 caracteres!' }),
 })
 
-export const SignInvalidation = z.object({
+export const SignInValidation = z.object({
     email: z.string().min(2, { message: 'Um Email é obrigatório' }).email({ message: 'Email inválido' }),
     password: z.string().min(6, { message: 'Sua senha precisa ter ao menos 6 caracteres!' }),
 })
