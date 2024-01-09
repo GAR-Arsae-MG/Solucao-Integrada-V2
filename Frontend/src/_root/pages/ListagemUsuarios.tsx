1946859f74a193f5f2883c53dd6384511ebb7717
darlanZero <darliankeira229@gmail.com>
2024-01-09 13:35:26 -0300

    reordening frontend

--- Frontend/src/_root/pages/ListagemUsuarios.tsx
+++ Frontend/src/_root/pages/ListagemUsuarios.tsx
@@ -1,41 +1,17 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback } from 'react'
 import TopNav from '../../components/ui/TopNav'
-import { Card, CardBody, Select, SelectItem, Spinner, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip, User } from '@nextui-org/react'
+import { Card, CardBody, Spinner, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip, User } from '@nextui-org/react'
 import { columns } from '../../components/data'
 import { EyeIcon } from '../../components/ui/EyeIcon';
 import { EditIcon } from '../../components/ui/EditIcon';
 import { DeleteIcon } from '../../components/ui/DeleteIcon';
 import { IGetUser } from '../../../types/types';
-import { useGetUsersFilters, useGetUsers } from '../../../react-query/QueriesAndMutations';
-import { getFuncoes, getUsersFilters } from '../../../django/api';
-
+import { useGetUsers } from '../../../react-query/QueriesAndMutations';
 
 
 function ListagemUsuarios() {
-    const [selectedFuncao, setSelectedFuncao] = useState('')
-    const [funcoes, setFuncoes] = useState<string[]>([])
-    //const [isStaff, setIsStaff] = useState('')
-
-    useEffect(() => {
-        const fetchFuncoes = async () => {
-            const funcoes = await getFuncoes()
-            setFuncoes(funcoes)
-        }
-        fetchFuncoes()
-    })
 
     const { data: users, isLoading, isError } = useGetUsers()
-    
-    //const {data: filtrosIsStaff, isLoading: isStaffLoading, isError: isStaffError } = useGetUsersFilters({ is_staff: isStaff})
-
-    const handleSelectChange = (value: string) => {
-        setSelectedFuncao(value);
-
-        getUsersFilters(value)
-    };
-
-    const { isLoading: isFuncaoLoading, isError: isFuncaoError } = useGetUsersFilters({ funcao: selectedFuncao})
-
 
  const renderCell = useCallback((user:IGetUser , columnKey: React.Key) => {
         let cellValue = user[columnKey as keyof IGetUser]
@@ -138,62 +114,6 @@ function ListagemUsuarios() {
         <TopNav />
 
         <div className='flex flex-col w-full items-center gap-4 p-4 min-h-screen from-purple-900 via-indigo-800 to-indigo-500 bg-gradient-to-tr'>
-            <Card className='max-w-full w-[1200px]'>
-                <CardBody className='overflow-auto scrollbar-hide'>
-                    <div>
-                        <p className='text-3xl font-bold text-gray-900 flex flex-col items-center text-center gap-4 p-4'>Listagem de Usuários</p>
-                    </div>
-
-                    <div className='flex flex-col'>
-                        <div className='inline-flex justify-between gap-4' >
-                            <Select
-                                label='Funcão'
-                                color='primary'
-                                value={selectedFuncao}
-                                onChange={(e) => handleSelectChange(e.target.value)}
-                            >
-                               {isFuncaoLoading ? (
-                                    <p>Carregando...</p>
-                               ): isFuncaoError ? (
-                                    <p>Erro ao buscar as agências.</p>
-                               ): (
-                                    funcoes.map((funcao) => (
-                                        <SelectItem
-                                            key={funcao}
-                                            value={funcao}
-                                        >
-                                            {funcao}
-                                        </SelectItem>
-                                    ))
-                               )}
-                            </Select>
-
-                            { /*<Select
-                                label='Admin de Sistema'
-                                color='success'
-                                value={isStaff.toString()}
-                                onChange={(e) => setIsStaff(e.target.value)}
-                            >
-                                {isStaffLoading ? (
-                                    <p>Carregando...</p>
-                                ): isStaffError ? (
-                                    <p>Erro ao buscar as opções.</p>
-                                ) : (
-                                    isStaffOptions.map((option) => (
-                                        <SelectItem
-                                            key={option.value}
-                                            value={option.value}
-                                        >
-                                            {option.label}
-                                        </SelectItem>
-                                    ))
-                                )}
-                            </Select> */}
-                        </div>
-                    </div>
-                </CardBody>
-            </Card>
-
             <Card className='max-w-full w-[1200px] h-[680px]'>
                 <CardBody className='overflow-auto scrollbar-hide'>
                     <Table aria-label='Tabela de usuários Dinâmica'>
